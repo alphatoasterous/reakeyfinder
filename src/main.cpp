a03e6b7e7f827f683490b0a7a63b7d44a478d657
ampilda <fluttershyred1926@gmail.com>
2025-05-21 19:13:52 +0300

    Create dev template

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#define REAPERAPI_IMPLEMENT
+
+#include <WDL/wdltypes.h> // might be unnecessary in future
+
+#include <reaper_plugin_functions.h>
+
+#include "my_plugin.h"
+
+extern "C"
+{
+    REAPER_PLUGIN_DLL_EXPORT auto REAPER_PLUGIN_ENTRYPOINT(REAPER_PLUGIN_HINSTANCE hInstance, reaper_plugin_info_t* rec)
+        -> int
+    {
+        PROJECT_NAME::hInstance = hInstance;
+        if (rec != nullptr && REAPERAPI_LoadAPI(rec->GetFunc) == 0)
+        {
+            // check that our plugin hasn't been already loaded
+            if (rec->GetFunc("ReaScriptAPIFunctionExample"))
+                return 0;
+            PROJECT_NAME::Register();
+            return 1;
+        }
+        // quit
+        PROJECT_NAME::Unregister();
+        return 0;
+    }
+}
